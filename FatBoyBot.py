0c945481f0bf77d7d41bf125a4770743855afc02
Daniel <luzhanxun.daniel@gmail.com>
2017-06-27 14:44:01 -0700

    add wetchat message functions

---
+++ FatBoyBot.py
@@ -0,0 +1,48 @@
+# Author DL
+from FatBoySpider import FatBoySpider
+from scrapy.crawler import CrawlerProcess
+import itchat, time
+from itchat.content import *
+
+class FatBoyBot(object):
+    def __init__(self):
+        itchat.auto_login(True)
+        itchat.run()
+
+    @itchat.msg_register([TEXT, MAP, CARD, NOTE, SHARING])
+    def text_reply(msg):
+        message_text = msg['Text']
+        reply_text = FatBoyBot.process_message(message_text, msg['FromUserName'])
+        itchat.send('%s: %s' % (msg['Type'], reply_text), msg['FromUserName'])
+
+    @itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO])
+    def download_files(msg):
+        msg['Text'](msg['FileName'])
+        return '@%s@%s' % ({'Picture': 'img', 'Video': 'vid'}.get(msg['Type'], 'fil'), msg['FileName'])
+
+    @itchat.msg_register(TEXT, isGroupChat=True)
+    def text_reply(msg):
+        if msg['isAt']:
+            itchat.send(u'@%s\u2005I received: %s' % (msg['ActualNickName'], msg['Content']), msg['FromUserName'])
+
+    @staticmethod
+    def process_message(message, receiver):
+        if('干活' in str(message)):
+            return FatBoyBot.run_scraper(None, None, None, receiver)
+        else:
+            return FatBoyBot.bullshit_reply(message)
+
+    @staticmethod
+    def bullshit_reply(message):
+        return 'Fuck you'
+
+    @staticmethod
+    def run_scraper(source, start_date, end_date, receiver):
+        process = CrawlerProcess({
+            'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+            'FEED_FORMAT': 'json',
+            'FEED_URI': '_data/craigslist.json'
+        })
+        process.crawl(FatBoySpider)
+        process.start()  # the script will block here until the crawling is finished
+        return 'Go to https://daniellu.github.io/FBL/ to view the results'
