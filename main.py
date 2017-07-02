8a6ce397fc7e5d2b61551280ff76bc1947c88eb5
zlu <luzhanxun.daniel@gmail.com>
2017-07-02 10:44:03 -0700

    disable wechat for now

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # Author DL
 from scrapy import cmdline
 import os
-from FatBoyBot import FatBoyBot
+#from FatBoyBot import FatBoyBot
 
 if os.path.exists('_data/craigslist.json'):
     os.remove('_data/craigslist.json')
 else:
     print("No data file exist")
 
-bot = FatBoyBot()
+#bot = FatBoyBot()
+
+command = "scrapy runspider FatBoySpider.py -o _data/craigslist.csv -t csv"
+cmdline.execute(command.split())
